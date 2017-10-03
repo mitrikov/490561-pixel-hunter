3e6eb0ecb090465c174a7bbd0ccfd1dbe4070eb9
vokirtim <j6p@bk.ru>
2017-10-04 08:07:28 +1200

    Задание №2 пробный вариант
    Здесь реализован весь функционал задания за исключением кнопки "назад". Если в модуле stats.js раскоментировать import greeting from './greeting.js'
    stats.querySelector(`.back`).addEventListener(`click` showScreen.bind(null, greeting), false);
    то браузер выдает ошибку: Reference Error: greeting$1 is not defined.
    
    Также в модуле intro.js в комментариях написал альтернативный вариант, при котором модули будут меньше зависеть друг от друга

--- js/main.js
+++ js/main.js
@@ -1,40 +1,13 @@
-const activeScreen = document.getElementsByTagName(`main`)[0];
-const screens = [`greeting`, `rules`, `game-1`, `game-2`, `game-3`, `stats`].map((id) => document.getElementById(id));
-let currentScreenNum = 0;
+import intro from './intro.js';
+import showScreen from './screen.js';
 
-const ARROW_LEFT = 0x25;
-const ARROW_RIGHT = 0x27;
 
-const showScreen = (number) => {
-  activeScreen.innerHTML = screens[number].innerHTML;
-  currentScreenNum = number;
-};
+showScreen(intro);
 
-const showNextScreen = () => {
-  if (currentScreenNum < screens.length - 1) {
-    currentScreenNum++;
-    showScreen(currentScreenNum);
-  }
-};
+/* Альтернативный вариант - передавать объектом отдельно код и обработчики событий
+import {intro, setEventListeners} from './intro.js';
+import greeting from "./greeting";
 
-const showPreviousScreen = () => {
-  if (currentScreenNum > 0) {
-    currentScreenNum--;
-    showScreen(currentScreenNum);
-  }
-};
-
-const listenKeys = (e) => {
-  if (e.altKey && e.keyCode === ARROW_LEFT) {
-    e.preventDefault();
-    showPreviousScreen();
-  }
-
-  if (e.altKey && e.keyCode === ARROW_RIGHT) {
-    e.preventDefault();
-    showNextScreen();
-  }
-};
-
-showScreen(0);
-document.addEventListener(`keydown`, (e) => listenKeys(e), false);
+showScreen(intro);
+setEventListeners(greeting)
+*/
