694335035a9e9df1165eedf6f5a7cc33321e6dc6
vokirtim <j6p@bk.ru>
2017-10-04 21:08:07 +1200

    No commit

--- js/showScreen.js
+++ js/showScreen.js
@@ -1,3 +1,19 @@
+import intro from './intro';
+import greeting from './greeting';
+import rules from './rules';
+import game1 from './game1';
+import game2 from './game2';
+import game3 from './game3';
+import stats from './stats';
+
+export const SCREEN_INTRO = intro;
+export const SCREEN_GREETING = greeting;
+export const SCREEN_RULES = rules;
+export const SCREEN_FIRST_GAME = game1;
+export const SCREEN_SECOND_GAME = game2;
+export const SCREEN_THIRD_GAME = game3;
+export const SCREEN_STATS = stats;
+
 const activeScreen = document.querySelector(`main.central`);
 
 const showScreen = (screen) => {
