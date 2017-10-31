848d759929ebd5e1a40efa645a163523389e22e8
vokirtim <j6p@bk.ru>
2017-10-31 20:47:44 +1200

    Исправил тесты

--- js/classes/timer.test.js
+++ js/classes/timer.test.js
@@ -6,7 +6,7 @@ describe(`Объект Timer`, () => {
   it(`Метод tick уменьшает время на 1 единицу`, () => {
     let timer = new Timer(30);
     timer.tick();
-    assert.equal(timer.getValue(), 29);
+    assert.equal(timer.value, 29);
   });
 
   it(`Метод tick при последовательном вызове 5 раз уменьшает время на 5 единиц`, () => {
@@ -16,7 +16,7 @@ describe(`Объект Timer`, () => {
     timer.tick();
     timer.tick();
     timer.tick();
-    assert.equal(timer.getValue(), 25);
+    assert.equal(timer.value, 25);
   });
 
   it(`Таймер сообщает о том, что он закончен`, () => {
@@ -26,8 +26,8 @@ describe(`Объект Timer`, () => {
   });
 
   it(`Таймер прекращает отсчёт по достижению нуля`, () => {
-    let timer = new Timer(0);
+    let timer = new Timer(1);
     timer.tick();
-    assert.equal(timer.getValue(), 0);
+    assert.equal(timer.value, 0);
   });
 });
